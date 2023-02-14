df0274153127a0c1267134f4936b76cff84ad5a7
Marian <mariana.escemi@gmail.com>
2023-02-14 01:23:59 -0300

    app de notas

---
+++ src/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { MONGODB_URI } from "./config.js";
+
+try {
+  const db = await mongoose.connect(MONGODB_URI);
+  console.log("Conectado a ", db.connection.name);
+} catch (error) {
+  console.error(error);
+}
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose está Conectado");
+});
+
+mongoose.connection.on("Desconectado", () => {
+  console.log("Mongoose está Desconectado");
+});
+
